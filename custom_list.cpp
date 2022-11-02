1caa95f086cdcd17f609b886bb32ccbf0f687f34
Roman Herasymenko <roman.herasymenko@gmail.com>
2022-11-02 18:35:45 +0000

    Added removeNode.

--- custom_list.cpp
+++ custom_list.cpp
@@ -30,7 +30,7 @@ public:
         ++count;
     }
 
-    DataObject* search(unsigned int idObject)
+    DataObject *search(unsigned int idObject)
     {
         return dataVector[idObject].get();
     }
@@ -52,14 +52,15 @@ private:
 };
 
 // A linked list node
-class Node {
+class Node
+{
 public:
     int data;
 
     std::shared_ptr<Node> next;
     std::shared_ptr<Node> prev;
 };
- 
+
 /* Given a reference (pointer to pointer)
 to the head of a list
 and an int, inserts a new node on the
@@ -68,50 +69,51 @@ std::shared_ptr<Node> push(std::shared_ptr<Node> head_ref, int new_data)
 {
     /* 1. allocate node */
     std::shared_ptr<Node> new_node = std::make_shared<Node>();
- 
+
     /* 2. put in the data */
     new_node->data = new_data;
- 
+
     /* 3. Make next of new node as head
     and previous as NULL */
     new_node->next = head_ref;
     new_node->prev = nullptr;
- 
+
     /* 4. change prev of head node to new node */
     if (head_ref.get() != nullptr)
     {
         head_ref->prev = new_node;
     }
- 
+
     /* 5. move the head to point to the new node */
     return new_node;
 }
- 
+
 /* Given a node as prev_node, insert
 a new node after the given node */
 std::shared_ptr<Node> insertAfter(std::shared_ptr<Node> prev_node, int new_data)
 {
     /*1. check if the given prev_node is NULL */
-    if (prev_node.get() == nullptr) {
+    if (prev_node.get() == nullptr)
+    {
         std::cout << "the given previous node cannot be NULL" << std::endl;
         return nullptr;
     }
- 
+
     /* 2. allocate new node */
     std::shared_ptr<Node> new_node = std::make_shared<Node>();
- 
+
     /* 3. put in the data */
     new_node->data = new_data;
- 
+
     /* 4. Make next of new node as next of prev_node */
     new_node->next = prev_node->next;
- 
+
     /* 5. Make the next of prev_node as new_node */
     prev_node->next = new_node;
- 
+
     /* 6. Make prev_node as previous of new_node */
     new_node->prev = prev_node;
- 
+
     /* 7. Change previous of new_node's next node */
     if (new_node->next != nullptr)
     {
@@ -120,44 +122,68 @@ std::shared_ptr<Node> insertAfter(std::shared_ptr<Node> prev_node, int new_data)
 
     return new_node;
 }
- 
+
+void removeNode(std::shared_ptr<Node> node_to_remove)
+{
+    /*1. check if the given prev_node is NULL */
+    if (node_to_remove.get() == nullptr)
+    {
+        std::cout << "the given previous node cannot be NULL" << std::endl;
+        return;
+    }
+
+    auto prev_node = node_to_remove->prev;
+    auto next_node = node_to_remove->next;
+
+    if (prev_node.get() != nullptr)
+    {
+        prev_node->next = next_node;
+    }
+
+    if (next_node.get() != nullptr)
+    {
+        next_node->prev = prev_node;
+    }
+}
+
 /* Given a reference (pointer to pointer) to the head
 of a DLL and an int, appends a new node at the end */
 std::shared_ptr<Node> append(std::shared_ptr<Node> head_ref, int new_data)
 {
     /* 1. allocate node */
     std::shared_ptr<Node> new_node = std::make_shared<Node>();
- 
+
     /* 2. put in the data */
     new_node->data = new_data;
- 
+
     /* 3. This new node is going to be the last node, so
         make next of it as NULL*/
     new_node->next = nullptr;
- 
+
     /* 4. If the Linked List is empty, then make the new
         node as head */
-    if (head_ref.get() == nullptr) {
+    if (head_ref.get() == nullptr)
+    {
         new_node->prev = nullptr;
         return new_node;
     }
- 
+
     /* 5. Else traverse till the last node */
     std::shared_ptr<Node> last = head_ref; /* used in step 5*/
     while (last->next != nullptr)
     {
         last = last->next;
     }
- 
+
     /* 6. Change the next of last node */
     last->next = new_node;
- 
+
     /* 7. Make last node as previous of new node */
     new_node->prev = last;
- 
+
     return new_node;
 }
- 
+
 // This function prints contents of
 // linked list starting from the given node
 void printList(std::shared_ptr<Node> node)
@@ -165,46 +191,52 @@ void printList(std::shared_ptr<Node> node)
     std::shared_ptr<Node> last;
     std::cout << "\nTraversal in forward direction \n";
 
-    while (node.get() != nullptr) {
+    while (node.get() != nullptr)
+    {
         std::cout << " " << node->data << " ";
         last = node;
         node = node->next;
     }
- 
+
     std::cout << "\nTraversal in reverse direction \n";
-    while (last.get() != nullptr) {
+    while (last.get() != nullptr)
+    {
         std::cout << " " << last->data << " ";
         last = last->prev;
     }
 }
- 
+
 // Driver code
 int main()
 {
     /* Start with the empty list */
     std::shared_ptr<Node> head = std::make_shared<Node>();
- 
+
     // Insert 6. So linked list becomes 6->NULL
     append(head, 6);
- 
+
     // Insert 7 at the beginning. So
     // linked list becomes 7->6->NULL
     head = push(head, 7);
- 
+
     // Insert 1 at the beginning. So
     // linked list becomes 1->7->6->NULL
     head = push(head, 1);
- 
+
     // Insert 4 at the end. So linked
     // list becomes 1->7->6->4->NULL
     append(head, 4);
- 
+
     // Insert 8, after 7. So linked
     // list becomes 1->7->8->6->4->NULL
-    insertAfter(head->next, 8);
- 
+    auto number_eight = insertAfter(head->next, 8);
+
     std::cout << "Created DLL is: ";
     printList(head);
- 
+
+    removeNode(number_eight);
+    std::cout << "After removal: ";
+    printList(head);
+
     return 0;
 }
